6736a3077b0bb98e5f66a3658331a84d79cc1c78
steven cardwell <flashbladez@users.noreply.github.com>
2018-11-07 15:35:31 +0000

    Add files via upload

---
+++ DistanceGameObjectManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WarQuest.Environment
+{
+    public class DistanceGameObjectManager : MonoBehaviour
+    {
+
+       
+        [SerializeField] float maxDistanceToDeactivate;
+
+        DistanceGameObjectsConfig distanceGameObjectConfig;
+        GameObject[] gameObjectToDeactivate;
+
+        void Start()
+        {
+            distanceGameObjectConfig = GameObject.FindGameObjectWithTag("DistanceObjects").GetComponent<DistanceGameObjectsConfig>();
+        }
+
+
+        void Update()
+        {
+            gameObjectToDeactivate = distanceGameObjectConfig.GameObjectsToDeactivate();
+        
+            for (int i = 0; i < gameObjectToDeactivate.Length; i++)
+            {
+              //   var playerPositionWorldSpace = transform.TransformPoint(transform.position);
+                var distanceToGameObject = Vector3.Distance(gameObjectToDeactivate[i].transform.position, transform.position);
+              
+                if (distanceToGameObject > maxDistanceToDeactivate)
+                {
+                    gameObjectToDeactivate[i].SetActive(false);
+                }
+                else
+                {
+                    gameObjectToDeactivate[i].SetActive(true);
+                }
+            }
+        }
+    }
+}
